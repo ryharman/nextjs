5a6abd53f07a97c3f279c3e96e522ca50a8792da
Ryan Harman <ryan.harman63@gmail.com>
2021-04-15 20:01:28 +0100

    ok final one i promise

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Head from "next/head";
+import About from "../components/About/Index";
 import Hello from "../components/Greeting/Index";
 import PageHeader from "../components/PageHeader/Index";
 import Waves from "../components/Waves/Index";
-import { BackgroundOverwrite, MainWrapper, Name } from "../styles/home";
+import { BackgroundOverwrite } from "../styles/BackgroundOverwrite";
+import {
+  BackgroundWhite,
+  CentreContent,
+  MainWrapper,
+  Name,
+} from "../styles/global";
 
 export default function Home() {
   return (
@@ -13,14 +20,19 @@ export default function Home() {
       </Head>
       <PageHeader />
       <MainWrapper>
-        <Hello />
-        <Name>Ryan Harman</Name>
-        <h3>
-          A Software Developer based in Chester currently working at Bentley
-          Motors{" "}
-        </h3>
+        <CentreContent>
+          <Hello />
+          <Name>Ryan Harman</Name>
+          <h3>
+            A Software Developer based in Chester currently working at Bentley
+            Motors{" "}
+          </h3>
+        </CentreContent>
       </MainWrapper>
       <Waves />
+      <BackgroundWhite>
+        <About />
+      </BackgroundWhite>
     </BackgroundOverwrite>
   );
 }
