c84b908ac0008e8cb2df4f05b9e199a0d75cbdc0
Ryan Harman <ryan.harman63@gmail.com>
2021-04-15 13:17:14 +0100

    style fix 3

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Hello from "../components/Greeting/Index";
 import PageHeader from "../components/PageHeader/Index";
 import Waves from "../components/Waves/Index";
-import { BackgroundOverwrite, MainWrapper, Name } from "../styles/global";
+import { BackgroundOverwrite, MainWrapper, Name } from "../components/Styled/global";
 
 export default function Home() {
   return (
